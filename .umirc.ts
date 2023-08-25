28492e4ed65f194ed476ca2633d9346985c985c2
Chushen <yangtao.yangtao@antgroup.com>
2023-08-25 19:06:49 +0800

    chore: update meta info

--- .umirc.ts
+++ .umirc.ts
@@ -11,4 +11,15 @@ export default defineConfig({
     'https://www.googletagmanager.com/gtag/js?id=G-7XLKHE56WW',
     "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-7XLKHE56WW');",
   ],
+  metas: [
+    {
+      name: 'keywords',
+      content:
+        'knowledge graph, 知识图谱, 可编程知识图谱, SPG, Semantic-Enhanced Programming Graph',
+    },
+    {
+      name: 'description',
+      content: '语义增强可编程知识图谱SPG',
+    },
+  ],
 });
